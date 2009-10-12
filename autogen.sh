803fce60057573c99030cd339cfe2e1359c78eec
Andrea Piccinelli <frasten@gmail.com>
2009-10-13 00:07:20 +0200

    Aggiunta l'opzione clean a autogen.sh, per eliminare i files generati automaticamente.

--- autogen.sh
+++ autogen.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
-autoreconf -f -i
-
-if [ !"$NOCONFIGURE" ]; then
-	./configure $@
+if [ "$1" = "clean" ]; then
+	if [ -f "Makefile" ]; then
+		make distclean
+	fi
+	rm -rf *.in *.la *.lo *~ aclocal.m4 autom4te.cache/ config.guess \
+		config.h config.log config.status config.sub configure depcomp \
+		install-sh libtool ltmain.sh missing .deps .libs stamp-h1
+	exit
 fi
+exit
+autoreconf -f -i && ./configure $@
