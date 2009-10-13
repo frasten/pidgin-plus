8a38b06bb3178ab9be3e35789b8c15494cead8a2
Andrea Piccinelli <frasten@gmail.com>
2009-10-13 02:24:17 +0200

    Added INSTALL to the files autogen.sh should remove.

--- autogen.sh
+++ autogen.sh
@@ -3,7 +3,7 @@ if [ "$1" = "clean" ]; then
 	if [ -f "Makefile" ]; then
 		make distclean
 	fi
-	rm -rf *.in *.la *.lo *~ aclocal.m4 autom4te.cache/ config.guess \
+	rm -rf INSTALL *.in *.la *.lo *~ aclocal.m4 autom4te.cache/ config.guess \
 		config.h config.log config.status config.sub configure depcomp \
 		install-sh libtool ltmain.sh missing .deps .libs stamp-h1
 	exit
